1f5d9accb973d74b2f6f7487a7b086c15aa9553b
tom@home <thomas.winkler@iggmp.net>
2023-03-29 18:57:57 +0200

    fixed send, added crc8 checksum

--- src/serialcan.cpp
+++ src/serialcan.cpp
@@ -5,22 +5,122 @@
 void SERIALCAN::begin(long baud) {
 
 	Serial.begin(baud);
+
+	_reset();
 }
 
 
 // message is available
 bool SERIALCAN::available(void) {
 
+	uint8_t byte;
+
+	if (Serial.available()) {
+
+		// add data to buffer
+		if (_counter < SERIALCAN_MAX_BYTES) {
+
+			if (_check_start(byte)) {
+				// _message[_counter++] = byte;
+			}
+		}
+
+		// message is too long -> discard
+		else {
+			_reset();
+			return false;
+		}
+	}
 }
 
 
 // send message to serial bus
-bool send(CAN_MESSAGE* buffer) {
+bool SERIALCAN::send(CAN_MESSAGE* message) {
 
+	uint8_t length;
+
+	length = _message2buffer(message);
+
+	// Serial.write(length);
+	Serial.write(_buffer, 18);
 }
 
 
 // read message
-uint8_t read(CAN_MESSAGE* buffer) {
+CAN_MESSAGE SERIALCAN::read(void) {
 
+}
+
+
+// reset read counter
+void SERIALCAN::_reset(void) {
+
+	_counter = 0;
+	_startbytes = 0;
+	_reading = false;
+}
+
+
+// check if message has started
+// SERIALCAN_START_BYTES SERIALCAN_START_VALUE received
+bool SERIALCAN::_check_start(uint8_t byte) {
+
+	if (byte == SERIALCAN_START_VALUE) {
+
+		if (_startbytes < SERIALCAN_START_BYTES) {
+
+			_startbytes++;
+			return false;
+		}
+
+		else {
+			return true;
+		}
+	}
+}
+
+
+// convert message to byte buffer array
+uint8_t SERIALCAN::_message2buffer(CAN_MESSAGE* message) {
+
+	// write start bytes
+	for (_i = 0; _i < SERIALCAN_START_BYTES; _i++) {
+		_buffer[_i] = SERIALCAN_START_VALUE;
+	}
+
+	// add size
+	_buffer[_i++] = message->size;
+
+	// add can-id
+	_buffer[_i++] = (message->id >> 24) & 0xFF;
+	_buffer[_i++] = (message->id >> 16) & 0xFF;
+	_buffer[_i++] = (message->id >> 8) & 0xFF;
+	_buffer[_i++] = message->id & 0xFF;
+
+	// add uuid
+	_buffer[_i++] = (message->uuid >> 8) & 0xFF;
+	_buffer[_i++] = message->uuid & 0xFF;
+
+	// add data bytes
+	for (_j = 0; _j < message->size; _j++) {
+		_buffer[_i + _j] = message->data[_j];
+	}
+
+	// add checksum
+	_buffer[_i + _j] = _checksum(_buffer, _i + _j);
+
+	return _i + _j;
+}
+
+
+// calculate checksum of array
+uint8_t SERIALCAN::_checksum(uint8_t* data, uint8_t length) {
+
+	uint8_t sum = 0;
+
+	for(_i = 0; _i < length; _i++) {
+		sum ^= data[_i];
+	}
+
+	return sum;
 }
--- src/serialcan.h
+++ src/serialcan.h
@@ -7,21 +7,25 @@
 /*
  * Message from serial port:
  * 		starts with 4 bytes 0xFF
- *		count of overall bytes of the message (8 bit)
- *		id (16/32 bit) depending on 11 or 29 bit id
- *		[uuid (16 bit)]
- *		data (up to 8 bytes)
  *		data size (8 bit)
+ *		id (4 bytes)
+ *		uuid (2 bytes)
+ *		data (up to 8 bytes)
  *
  * Minimum length: 
  */
+
+#define SERIALCAN_MAX_BYTES 15
+#define SERIALCAN_START_BYTES 2
+#define SERIALCAN_START_VALUE 0xFF
 
 
 struct CAN_MESSAGE {
     uint32_t id;
     uint16_t uuid;
-    uint8_t data[8];
     uint8_t size;
+    uint8_t data[8];
+    uint8_t checksum;
 };
 
 
@@ -32,11 +36,21 @@ public:
 	void begin(long baud);
 	bool available(void);
 	bool send(CAN_MESSAGE* buffer);
-	uint8_t read(CAN_MESSAGE* butter);
+	CAN_MESSAGE read(void);
 
 private:
 
+    uint8_t _i, _j; // global iterators
+    CAN_MESSAGE _message;
+    uint8_t _buffer[sizeof(CAN_MESSAGE) + SERIALCAN_START_BYTES + 1];
+    uint8_t _startbytes;
+    uint8_t _counter;
+    bool _reading;
 
+    void _reset(void);
+    bool _check_start(uint8_t);
+    uint8_t _message2buffer(CAN_MESSAGE*);
+    uint8_t _checksum(uint8_t*, uint8_t);
 };
 
 
